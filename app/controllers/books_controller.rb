4da5e0c562947e4c36f43063ae61a9c467384552
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-08-04 13:58:07 +0000

    [Add] show,edit,update,destroy追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,27 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path, notice: 'Book was successfully created.'
+    redirect_to book_path, notice: 'Book was successfully created.'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
